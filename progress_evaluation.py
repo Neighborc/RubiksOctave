86113f28bf4aa2511ee06ebecac2fee7cf0bdde2
Chris Neighbor <chrisjneighbor@gmail.com>
2021-08-02 19:33:27 -0700

    Fix typos and example calculations

--- progress_evaluation.py
+++ progress_evaluation.py
@@ -4,6 +4,5 @@
 Some issues may include rotational variation and speed.
 If there are X number of possible actions per turn then looking to a depth of K results in
 X^K possible end states which increases exponentially
-11^1 = 11, 11^2= 121, 11^3=1331, 11^4=14641
-    
+24^1 = 24, 24^2=576, 24^3=13824, 24^4=331776    
 """
