02f8fd4eb00f7d6eeaa458ab00431fe01e8cbc98
Chris Neighbor <chrisjneighbor@gmail.com>
2021-08-04 16:36:47 -0700

    Graph array and search space reduction ideas

--- progress_evaluation.py
+++ progress_evaluation.py
@@ -4,5 +4,9 @@
 Some issues may include rotational variation and speed.
 If there are X number of possible actions per turn then looking to a depth of K results in
 X^K possible end states which increases exponentially
-24^1 = 24, 24^2=576, 24^3=13824, 24^4=331776    
+24^1 = 24, 24^2=576, 24^3=13824, 24^4=331776 
+
+Explore method for rapidly checking graph structure to be similar.
+Eventual profiling to work. Solve slightly shuffled cubes first.
 """
+
